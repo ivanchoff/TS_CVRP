d9ae38bb48577f28f7e2bad947e2ca72a4486c0d
Choff <ivanchoff@gmail.com>
2015-01-20 23:11:49 -0500

    globar struct data

--- TS.cc
+++ TS.cc
@@ -1,49 +1,15 @@
 #include<bits/stdc++.h>
-
 /*
-Principios de TS:
-->uso de estructuras de memoria flexibles
-->mecanismo asociado de control
-->funciones de memoria de diferentes lapsos de tiempo
-  -->memoria a corto plazo (lista tabu): historial de los
-     los ultimos movimientos realizados.
-  -->memoria mediano plazo: atributos mas comunes de un 
-     conjunto de soluciones
-  -->memoria largo plazo:registro de zonas que no han sido
-     exploradas aun.
-
-->Estrategia de vecindad:
-* operador lambda-intercambio
-* mutacion
-* insercion
-* 2-opt -intercambio
-
-
--->CARACTERISTICAS:
-
----->vector de solucion 
----->matriz de costos
----->lista de demanda
----->lista tabu:
-     -soluciones visitadas recientemente ||
-     -movimientos realizados recientemente ||
-     -atributos o caracteristicas que tenian soluciones visitadas
----->criterio de aspiracion
-
-*/
-
-
-/*
-
- * Definir criterio de aspiracion y correr el problema del pdf
- * agregar estrategia de intensificacion
-
+ * hacer las etapas de instensificacion terminando cuando no haya una
+ * mejor solucion; e implementar la etapa de divesificacion elijiendo 
+ * un vecino de la lista de posibles soluciones.
  */
 using namespace std;
 
-#define Q 20000 //capacidad del carro
+#define Q 23000 //capacidad del carro
 #define k 8  //cantidad de carros
 #define tabu_ternure 5  //tabu list size tabu ternure
+#define div_list_size 20 //lenght of diversification list
 
 struct tabu{
   int x;
@@ -54,52 +20,59 @@ struct tabu{
 typedef double db;
 typedef vector<db> vd;
 typedef vector<vector<db>> vvd;
+typedef map<db,vd> mvd;
 typedef vector<tabu> vt;
+typedef set<vector<double> > svd;
 
 void aspiration();
-int is_tabu_move(int x, int y, int pos_x, int pos_y, vt &tabu_list);
+int is_tabu_move(int x, int y, int pos_x, int pos_y);
 void print_tabu_list(vt tabu_list);
-void add_tabu_nodes(int x, int y, int pos_x, int posy, vt &tabu_list);
-db neighbor_cost(vd n, vd &demand, vvd &costs);
+void add_tabu_nodes(int x, int y, int pos_x, int posy);
+void add_to_div_list(vd n, db cost);
+db neighbor_cost(vd n);
 void exchange(int i, int j, vd &n);
 void insertion(int i, int j, db a, vd &n);
-void get_neighbor(vd s, vd &demand, vvd &costs, db &best_cost, vd &best_s, vt &tabu_list, int i, int &it);
-void load_costs(vvd &c);
-void read_init_sol(vd &s);
-void load_demand(vd &x);
+void get_neighbor(int i, int &it);
+void load_costs();
+void read_init_sol();
+void load_demand();
 void print(vd x);
 void print_matriz(vvd &x);
 
+vd s;
+vt tabu_list;
+mvd div_list; //list for diversification contain the best 20th solution. 
+svd div_tabu; //set with before taken soluction
+vd demand;
+vvd costs;
+vd best_sol;
+db best_cost=1<<30;
 
 int main(){
-  vd s;
-  vt tabu_list;
-  vd demand;
-  vvd costs;
-  //db tabu_iter=5;
-
-  db best_cost=1<<30;
-  vd best_sol;
+ 
   int best_it=0;
   int max_it=1000000;
   int i=0;
   
-  read_init_sol(s);
+  read_init_sol();
   cout<<"sol.size: "<<s.size()<<endl;
-  load_costs(costs);
-  load_demand(demand);
+  load_costs();
+  load_demand();
   cout<<"demand.size: "<<demand.size()<<endl;
   cout<<"init solution:"<<endl;
   print(s);
-  cout<<endl<<"cost: "<<neighbor_cost(s,demand,costs)<<endl;
-
-  /*  
+  cout<<endl<<"cost: "<<neighbor_cost(s)<<endl;
+  //cout<<"get_neighbor"<<endl;
+  //get_neighbor(1,best_it);
+  //cout<<endl<<"cost: "<<neighbor_cost(s)<<endl;
+  
+   
   while(i++ < max_it){
     get_neighbor(s,demand,costs,best_cost,best_sol,tabu_list,i,best_it);
     cout<<"iteration "<<i<<" : ";
     //print(s);
   }
-  
+  /*
   cout<<"best solution in it="<<best_it<<endl;
   print(best_sol);
   cout<<"cost: "<<best_cost;
@@ -113,7 +86,7 @@ int aspiration(db &best_cost, db &n_cost){
   return 0;
 }
 //return 1 if is tabu move otherwise return 0
-int is_tabu_move(int x, int y, int pos_x, int pos_y, vt &tabu_list){
+int is_tabu_move(int x, int y, int pos_x, int pos_y){
   for(auto i : tabu_list){
     if( ((i.x==x && i.y==y) || (i.x==y && i.y==x)) &&  
         ((i.pos_x==pos_x && i.pos_y==pos_y) || (i.pos_x==pos_y && i.pos_y==pos_x))) return 1;
@@ -128,7 +101,7 @@ void print_tabu_list(vt tabu_list){
   }
 }
 //add 2 nodes to tabu list
-void add_tabu_nodes(int x, int y, int pos_x, int pos_y, vt &tabu_list){
+void add_tabu_nodes(int x, int y, int pos_x, int pos_y){
   tabu tmp;
   tmp.x = x;
   tmp.y = y;
@@ -143,14 +116,14 @@ void add_tabu_nodes(int x, int y, int pos_x, int pos_y, vt &tabu_list){
   }
 }
 //return 0 if is not valid solution otherwise return cost solution
-db neighbor_cost(vd n, vd &demand, vvd &costs){
+db neighbor_cost(vd n){
   db carga = 0;
   db costo = 0;
   
   for(db i=0; i<n.size(); i++){
     int to = n[i];
     if(carga > Q){
-      cout<<carga<<": supera la carga ";
+      //cout<<carga<<": supera la carga"<<endl;
       return 0;
     }
     if(to == 1){
@@ -160,7 +133,7 @@ db neighbor_cost(vd n, vd &demand, vvd &costs){
       int from = n[i-1];
       carga += demand[to-1];
       costo += costs[from-1][to-1];
-      cout<<"from: "<<from<<" to:"<<to<<" demanda: "<<demand[to-1]<<" costo:"<< costs[from-1][to-1]<<endl;
+      //cout<<"from: "<<from<<" to:"<<to<<" demanda: "<<demand[to-1]<<" costo:"<< costs[from-1][to-1]<<endl;
       } 
   }
   return costo;  
@@ -179,8 +152,17 @@ void exchange(int i, int j, vd &n){
   //cout<<"exchanging "<<i<<" "<<j<<endl;
 }
 
+//add neighbor to divesification list
+void add_to_div_list(vd n, db cost){
+  if(!div_tabu.count(n)){
+    if(!div_list.count(cost)){
+      div_list[cost]=n;
+      if(div_list.size()>div_list_size)div_list.erase(div_list.end());
+    }
+  }
+}
 //generate all neighbors and evuale each of them
-void get_neighbor(vd s, vd &demand, vvd &costs, db &best_cost, vd &best_s, vt &tabu_list, int i, int &it){
+void get_neighbor(int i, int &it){
   db local_best_cost=1<<30;
   db x=0,y=0; //nodes involucrated in best neighbor
   int pos_x=0, pos_y=0; //dir of x and y
@@ -194,8 +176,11 @@ void get_neighbor(vd s, vd &demand, vvd &costs, db &best_cost, vd &best_s, vt &t
       if( a!=1 && b!=1  && b != 58 && a!=58 && a!=b){
         //insertion(i,j,a,tmp);
         exchange(i,j,tmp);
-        db tmp_cost = neighbor_cost(tmp,demand,costs);
-        if(!is_tabu_move(a,b,i,j,tabu_list)){
+        db tmp_cost = neighbor_cost(tmp);
+        
+        if(tmp_cost!=0)add_to_div_list(tmp,tmp_cost);
+        
+        if(!is_tabu_move(a,b,i,j)){
           if(tmp_cost < local_best_cost && tmp_cost!=0){
             local_best_cost = tmp_cost;
             best_neighbor = tmp;
@@ -214,19 +199,19 @@ void get_neighbor(vd s, vd &demand, vvd &costs, db &best_cost, vd &best_s, vt &t
   }
   if(local_best_cost < best_cost){
     best_cost = local_best_cost;
-    best_s = best_neighbor;
+    best_sol = best_neighbor;
     it = i;
   }
   //cout<<"best neighbor: "<<endl;
   //print(best_neighbor);
   cout<<"cost: "<<local_best_cost<<"nodes: ("<<x<<","<<y<<")("<<pos_x<<","<<pos_y<<")"<<endl;
   s = best_neighbor;
-  add_tabu_nodes(x,y,pos_x,pos_y,tabu_list);
+  add_tabu_nodes(x,y,pos_x,pos_y);
 }
 
 
 //load matriz of costs
-void load_costs(vvd &c){
+void load_costs(){
   fstream fs("costs",ios_base::in);
   string line;
 
@@ -241,14 +226,14 @@ void load_costs(vvd &c){
         break;
       lineData.push_back(val);
     }
-    c.push_back(lineData);
+    costs.push_back(lineData);
   }
-  cout<<"costs size: "<<c[0].size()<<"X"<<c.size()<<endl;
+  cout<<"costs size: "<<costs[0].size()<<"X"<<costs.size()<<endl;
 }
 
 
 //read aproximated solution vector
-void read_init_sol(vd &s){
+void read_init_sol(){
   fstream fs("init_sol", ios_base::in);
   db x;
   while(fs >> x){
@@ -256,11 +241,11 @@ void read_init_sol(vd &s){
   }
 }
 //load demand vector
-void load_demand(vd &x){
+void load_demand(){
   fstream fs("demand", ios_base::in);
   db tmp;
   while(fs >> tmp){
-    x.push_back(tmp);
+    demand.push_back(tmp);
   }
 }  
 
